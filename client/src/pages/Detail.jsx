3f139e63002b3e1cb2c91cd0955daf66a9189d75
Kyler <klyerpeterson23@gmail.com>
2023-08-18 20:19:51 -0600

    applied redux to pages

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,83 +1,61 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-
+import { useDispatch, useSelector } from 'react-redux'; // Import Redux hooks
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
 import {
-  REMOVE_FROM_CART,
-  UPDATE_CART_QUANTITY,
-  ADD_TO_CART,
-  UPDATE_PRODUCTS,
-} from '../utils/actions';
+  removeFromCart,
+  updateCartQuantity,
+  addToCart,
+  updateProducts,
+} from '../utils/actions'; // Import your Redux action creators
+import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch(); // Get the dispatch function from Redux
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
+
+  const state = useSelector(state => state.productReducer); // Access product and cart state from Redux store
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   const { products, cart } = state;
 
   useEffect(() => {
-    // already in global store
     if (products.length) {
       setCurrentProduct(products.find((product) => product._id === id));
-    }
-    // retrieved from server
-    else if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
-
+    } else if (data) {
+      dispatch(updateProducts(data.products)); // Use the action creator to update products
       data.products.forEach((product) => {
         idbPromise('products', 'put', product);
       });
-    }
-    // get cache from idb
-    else if (!loading) {
+    } else if (!loading) {
       idbPromise('products', 'get').then((indexedProducts) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-          products: indexedProducts,
-        });
+        dispatch(updateProducts(indexedProducts)); // Use the action creator to update products
       });
     }
   }, [products, data, loading, dispatch, id]);
 
-  const addToCart = () => {
+  const addToCartHandler = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === id);
     if (itemInCart) {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
-      });
+      dispatch(updateCartQuantity(id, parseInt(itemInCart.purchaseQuantity) + 1)); // Use the action creator to update cart quantity
       idbPromise('cart', 'put', {
         ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
       });
     } else {
-      dispatch({
-        type: ADD_TO_CART,
-        product: { ...currentProduct, purchaseQuantity: 1 },
-      });
+      dispatch(addToCart({ ...currentProduct, purchaseQuantity: 1 })); // Use the action creator to add to cart
       idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
   };
 
-  const removeFromCart = () => {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: currentProduct._id,
-    });
-
+  const removeFromCartHandler = () => {
+    dispatch(removeFromCart(currentProduct._id)); // Use the action creator to remove from cart
     idbPromise('cart', 'delete', { ...currentProduct });
   };
 
@@ -93,10 +71,10 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
-            <button onClick={addToCart}>Add to Cart</button>
+            <button onClick={addToCartHandler}>Add to Cart</button>
             <button
               disabled={!cart.find((p) => p._id === currentProduct._id)}
-              onClick={removeFromCart}
+              onClick={removeFromCartHandler}
             >
               Remove from Cart
             </button>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
-import ProductList from "../components/ProductList";
+import React from "react";
+import { useSelector } from "react-redux"; // Import Redux hook
 import CategoryMenu from "../components/CategoryMenu";
+import ProductList from "../components/ProductList";
 import Cart from "../components/Cart";
 
 const Home = () => {
+  const state = useSelector(state => state.productReducer); // Access state from Redux store
+
   return (
     <div className="container">
       <CategoryMenu />
-      <ProductList />
+      <ProductList products={state.products} /> {/* Pass products as prop */}
       <Cart />
     </div>
   );
